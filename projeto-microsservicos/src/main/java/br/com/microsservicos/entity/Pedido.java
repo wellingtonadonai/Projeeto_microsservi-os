509e5b2bd77e2cd47a01523bf933226f305ca0ab
wellington <wellington.h.s.oliveira@hotmail.com>
2025-09-06 09:28:49 -0300

    tirei os imports desnecessario

--- projeto-microsservicos/src/main/java/br/com/microsservicos/entity/Pedido.java
+++ projeto-microsservicos/src/main/java/br/com/microsservicos/entity/Pedido.java
@@ -2,9 +2,7 @@ package br.com.microsservicos.entity;
 
 import br.com.microsservicos.enums.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
