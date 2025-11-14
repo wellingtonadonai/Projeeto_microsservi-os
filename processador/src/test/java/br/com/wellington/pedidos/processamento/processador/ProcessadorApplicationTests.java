1c6b2d34f4825488ae2fb132fc2ea04e9c15acec
wellington <wellington.h.s.oliveira@hotmail.com>
2025-11-13 21:29:01 -0300

    alterei nome de pasta

---
+++ processador/src/test/java/br/com/wellington/pedidos/processamento/processador/ProcessadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellington.pedidos.processamento.processador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
